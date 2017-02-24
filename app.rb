a8a70e3618e9bc9fd60caba4867be88d6aa0b863
hwalborn <hwalborn@gmail.com>
2017-02-24 10:30:50 -0500

    checkout

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "the secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[params.keys[0]] = params[:item]
+    @session = session
+    erb :show
+  end
 end
